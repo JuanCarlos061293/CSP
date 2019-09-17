b2a9c8c5160b3b600bfcacd8db18c34c22293b92
JuanCarlos061293 <55451851+JuanCarlos061293@users.noreply.github.com>
2019-09-17 10:31:31 -0500

    Update MuestreoMedal.java
    se añadio discrete_roulette

--- medal/muestreo/MuestreoMedal.java
+++ medal/muestreo/MuestreoMedal.java
@@ -8,4 +8,22 @@ public class MuestreoMedal implements Muestreo {
     public void createNewInd(LinkedList<Individuo> population, int[][] freqMatrix) {
 
     } // fin createNewInd
+    
+    protected int discrete_roulette(int[] frequencies){
+        double total = 0;
+        for (int i = 0; i < frequencies.length; i++)
+            total+=frequencies[i];
+
+        double random = Math.random();
+        double acumulator = 0;
+        for (int i = 0; i < frequencies.length; i++)
+        {
+            if( random >= acumulator && random <=  acumulator + frequencies[i] / total)
+                return (i+1);
+            acumulator += frequencies[i] / total;
+        }
+        //Nunca debería ser alcanzada esta línea. En caso de que sucediera las frecuencias están en ceros y hay un error.
+        return -1;
+    }// fin discrete_roulette
+    
 }// -->
